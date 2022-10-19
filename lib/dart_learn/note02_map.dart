0efee87deda712b1bd4cad337d60ce6cad97a21a
Your Name <you@example.com>
2022-10-19 10:39:56 +0800

    -

--- lib/dart_learn/note02_map.dart
+++ lib/dart_learn/note02_map.dart
@@ -2,16 +2,15 @@ import '../util/Log.dart';
 
 /// Map： 键值对集合，键不能重复，值可以重复
 main() {
-  ///基本属性
-// | 名称 | 说明 |
-// | —— | —— |
-// | isEmpty | 是否为空 |
-// | isNotEmpty | 是否不为空 |
-// | keys | key 集合 |
-// | values | values 集合 |
-// | length | 个数 |
-// | entries | 加工数据入口 |
+  ///1. 基本属性：
+  ///  | 名称 | 说明 |
+  ///  | isEmpty | 是否为空 |
+  ///  | isNotEmpty | 是否不为空 |
+  ///  | keys | key 集合 |
+  ///  | values | values 集合 |
+  ///  | length | 个数 |
 
+  ///2. 常用函数示例：
   Map map = {"k1": "k1-123"};
   Log.d("map.runtimeType ${map.runtimeType}");
 
@@ -38,8 +37,10 @@ main() {
   /// update()  根据key跟新数据： key 不存在会报异常
   map.update("s2", (val) => "s2-123");
   Log.d(map);
+
   /// 取值
   Log.d(map["s2"]);
+
   /// 赋值
   map["s2"] = "s2-999";
   Log.d(map);

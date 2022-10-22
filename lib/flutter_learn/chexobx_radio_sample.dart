358e730aad05038cc7412b4bca8063d864bf3ddc
Your Name <you@example.com>
2022-10-22 21:06:26 +0800

    -

--- lib/flutter_learn/chexobx_radio_sample.dart
+++ lib/flutter_learn/chexobx_radio_sample.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:my_flutter_lib_3/my_widgets/my_checkbox.dart';
 import 'package:provider/provider.dart';
 
-import '../util/Log.dart';
-
+/// 官方提供的 Checkbox， Radio， Switch 等控件不能完全灵活的自定义宽高，相对位置等，后期可能都需要自定义...
 /// 复选框 Checkbox, 用法示例
 /// 复选框 CheckboxListTile, 用法示例
 /// 单选框 Radio, 用法示例
+/// 开关 Switch, 用法示例
+/// 开关 SwitchListTile, 用法示例
 class CheckboxSamplePage extends StatefulWidget {
   const CheckboxSamplePage({super.key});
 
@@ -43,9 +44,10 @@ class _SamplePageState extends State {
             buildSizedBox1(),
             //   UIUtil.getEmptyBoxByHeight(10),
             buildSizedBox2(),
-
             buildSizedBox3(),
             buildSizedBox4(),
+            buildSizedBox5(),
+            buildSizedBox6(),
           ],
         ),
       ),
@@ -125,6 +127,7 @@ class _SamplePageState extends State {
                 Radio(
                     value: 0,
                     groupValue: notifier.intRadioValue,
+                    activeColor: Colors.green,
                     onChanged: (int? value) => notifier.setIntRadioValue(value ?? 0)),
                 Radio(
                     value: 1,
@@ -143,11 +146,74 @@ class _SamplePageState extends State {
   }
 }
 
+/// 2. Switch, 用法示例: Switch的value属性为true表示打，false表示关闭
+///    Switch 没有提供api Track Thumb的大小？？？
+SizedBox buildSizedBox5() {
+  return SizedBox(
+    child: Container(
+      height: 80,
+      width: 180,
+      color: Colors.greenAccent,
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(0),
+      child: Consumer<_MyValuesNotifier>(builder: (context, _MyValuesNotifier notifier, child) {
+        return Switch(
+          value: notifier.switchEnable,
+          // 打开时候 开关样式
+          activeColor: Colors.blue,
+          activeTrackColor: Colors.white,
+          // activeThumbImage: ,
+          // 关闭时候开关样式
+          inactiveThumbColor: Colors.white,
+          inactiveTrackColor: const Color.fromARGB(222, 175, 175, 175),
+          materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          onChanged: (bool value) {
+            notifier.setSwitchEnable(value);
+          },
+        );
+      }),
+    ),
+  );
+}
+
+/// 2. SwitchListTile, 用法示例: Switch的value属性为true表示打，false表示关闭
+///    SwitchListTile 没有提供api Track Thumb的大小？？？
+SizedBox buildSizedBox6() {
+  return SizedBox(
+    child: Container(
+      height: 80,
+      width: 280,
+      color: Colors.greenAccent,
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(0),
+      child: Consumer<_MyValuesNotifier>(builder: (context, _MyValuesNotifier notifier, child) {
+        return SwitchListTile(
+          value: notifier.switchListTileEnable,
+          // 打开时候 开关样式
+          activeColor: Colors.blue,
+          activeTrackColor: Colors.white,
+          // activeThumbImage: ,
+          // 关闭时候开关样式
+          inactiveThumbColor: Colors.white,
+          inactiveTrackColor: const Color.fromARGB(222, 175, 175, 175),
+          title: const Text("我是一级标题"),
+          subtitle: const Text("我是2级标题..."),
+          onChanged: (bool value) {
+            notifier.setSwitchListTileEnable(value);
+          },
+        );
+      }),
+    ),
+  );
+}
+
 class _MyValuesNotifier extends ChangeNotifier {
   int intValue = 0;
   int intRadioValue = 0;
   bool boolValue = false;
   bool boolValue2 = false;
+  bool switchEnable = false;
+  bool switchListTileEnable = false;
 
   void setIntValue(int value) {
     intValue = value;
@@ -165,8 +231,17 @@ class _MyValuesNotifier extends ChangeNotifier {
   }
 
   void setIntRadioValue(int value) {
-    Log.d("setIntRadioValue value: $value");
     intRadioValue = value;
+    notifyListeners();
+  }
+
+  void setSwitchEnable(bool value) {
+    switchEnable = value;
+    notifyListeners();
+  }
+
+  void setSwitchListTileEnable(bool value) {
+    switchListTileEnable = value;
     notifyListeners();
   }
 }

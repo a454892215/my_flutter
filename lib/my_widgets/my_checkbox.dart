d2818989b0ce560343e60946cd672fbdef9d2d52
Your Name <you@example.com>
2022-10-22 18:35:23 +0800

    图片抖动问题处理优化

--- lib/my_widgets/my_checkbox.dart
+++ lib/my_widgets/my_checkbox.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class MyCheckBox extends StatefulWidget {
   const MyCheckBox({super.key, required this.title});
@@ -8,62 +7,51 @@ class MyCheckBox extends StatefulWidget {
 
   @override
   State<StatefulWidget> createState() {
-    return MyCheckBoxState();
+    return _MyCheckBoxState();
   }
 }
 
-class MyCheckBoxState extends State<MyCheckBox> {
+class _MyCheckBoxState extends State<MyCheckBox> {
   late Image selectedImg;
   late Image unselectedImg;
+  bool checked = false;
 
   @override
   void initState() {
     selectedImg = Image.asset(
       'images/check/ic_checked.png',
       width: 50,
+      gaplessPlayback: true,
     );
     unselectedImg = Image.asset(
       'images/check/ic_uncheck.png',
       width: 50,
+      gaplessPlayback: true,
     );
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    Color curColor = checked ? Colors.green : Colors.black;
+    Image curImage = checked ? selectedImg : unselectedImg;
+
     /// GestureDetector无点击水波纹效果 InkWell有 。第一次点击会抖动一下？
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => _MyChangeNotifier()),
-        //  Provider(create: (context) => SomeOtherClass()),
-      ],
-      child: Consumer<_MyChangeNotifier>(builder: (context, notifier, child) {
-        Color curColor = notifier.checked ? Colors.green : Colors.black;
-        Image curImage = notifier.checked ? selectedImg : unselectedImg;
-        return InkWell(
-          child: Column(
-            children: [
-              curImage,
-              Text(
-                widget.title,
-                style: TextStyle(color: curColor),
-              )
-            ],
-          ),
-          onTap: () {
-            notifier.setCheck(!notifier.checked);
-          },
-        );
-      }),
+    return InkWell(
+      child: Column(
+        children: [
+          curImage,
+          Text(
+            widget.title,
+            style: TextStyle(color: curColor),
+          )
+        ],
+      ),
+      onTap: () {
+        setState(() {
+          checked = !checked;
+        });
+      },
     );
-  }
-}
-
-class _MyChangeNotifier extends ChangeNotifier {
-  bool checked = false;
-
-  void setCheck(bool value) {
-    checked = value;
-    notifyListeners();
   }
 }

e4780e44018a28d3985b681f118a3b2c491fc99a
Your Name <1573802540@qq.com>
2022-10-26 08:32:38 +0800

    AnimatedDefaultTextStyle 示例

--- lib/flutter_learn/note20_animate_sample.dart
+++ lib/flutter_learn/note20_animate_sample.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+String summary = ''' 
+1. AnimatedAlign
+2. AnimatedPositioned
+3. AnimatedPositionedDirectional
+4. AnimatedOpacity
+5. AnimatedDefaultTextStyle
+''';
+
 class AnimationSamplePage extends StatefulWidget {
   const AnimationSamplePage({super.key});
 
@@ -33,12 +41,13 @@ class _SamplePageState extends State with SingleTickerProviderStateMixin {
         ),
         body: Container(
           color: Colors.grey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
+          child: ListView(
             children: [
               buildAnimatedAlign(),
               buildAnimatedPositioned(),
+              buildAnimatedPositionedDirectional(),
+              buildAnimatedOpacity(),
+              buildAnimatedDefaultTextStyle(),
             ],
           ),
         ));
@@ -52,36 +61,35 @@ class _SamplePageState extends State with SingleTickerProviderStateMixin {
         curve: Curves.linear,
         duration: const Duration(milliseconds: 250),
         child: MaterialButton(
-          onPressed: () => notifier.playAlignAim(),
+          onPressed: () => notifier.playAnimatedAlign(),
           color: Colors.blue,
-          child: const Text("点击play-AnimatedAlign动画"),
+          child: const Text("AnimatedAlign"),
         ),
       );
     });
   }
 
-  /// 2. buildAnimatedPositioned
+  /// 2. AnimatedPositioned
   Widget buildAnimatedPositioned() {
     return Container(
-      height: 200,
+      height: 80,
       margin: const EdgeInsets.only(left: 10, right: 10, top: 10),
       color: Colors.purple,
       child: Stack(
         children: [
-          /// 1. 注意如果Positioned视图越界会抛异常，异常log难定位??
           Consumer<_MyValuesNotifier>(builder: (context, notifier, child) {
             return AnimatedPositioned(
               left: notifier.positionLeft,
               top: 10,
-              width: 100,
-              height: 100,
+              width: 160,
+              height: 50,
               duration: const Duration(milliseconds: 250),
               child: Container(
                 color: Colors.blue,
                 child: MaterialButton(
-                  onPressed: () => notifier.playAnimatedPositionedAnim(),
+                  onPressed: () => notifier.playAnimatedPositioned(),
                   color: Colors.blue,
-                  child: const Text("play-AnimatedPositioned-动画"),
+                  child: const Text("AnimatedPositioned"),
                 ),
               ),
             );
@@ -90,19 +98,117 @@ class _SamplePageState extends State with SingleTickerProviderStateMixin {
       ),
     );
   }
+
+  /// 3. AnimatedPositionedDirectional
+  Widget buildAnimatedPositionedDirectional() {
+    return Container(
+      height: 80,
+      margin: const EdgeInsets.only(left: 10, right: 10, top: 10),
+      color: Colors.purple,
+      child: Stack(
+        children: [
+          Consumer<_MyValuesNotifier>(builder: (context, notifier, child) {
+            return AnimatedPositionedDirectional(
+              start: notifier.positionStart,
+              top: 10,
+              width: 160,
+              height: 50,
+              duration: const Duration(milliseconds: 250),
+              child: Container(
+                color: Colors.blue,
+                child: MaterialButton(
+                  onPressed: () => notifier.playAnimatedPositionedStart(),
+                  color: Colors.blue,
+                  child: const Text("AnimatedPositionedDirectional"),
+                ),
+              ),
+            );
+          }),
+        ],
+      ),
+    );
+  }
+
+  /// 4. AnimatedOpacity
+  Widget buildAnimatedOpacity() {
+    return Consumer<_MyValuesNotifier>(builder: (context, notifier, child) {
+      return Align(
+        alignment: Alignment.topLeft,
+        child: AnimatedOpacity(
+          opacity: notifier.opacity,
+          curve: Curves.linear,
+          duration: const Duration(milliseconds: 250),
+          child: MaterialButton(
+            onPressed: () => notifier.playAnimatedOpacity(),
+            color: Colors.blue,
+            child: const Text("AnimatedOpacity"),
+          ),
+        ),
+      );
+    });
+  }
+
+  /// 5. AnimatedDefaultTextStyle
+  Widget buildAnimatedDefaultTextStyle() {
+    return Consumer<_MyValuesNotifier>(builder: (context, notifier, child) {
+      return Container(
+        height: 80,
+        color: Colors.orange,
+        margin: const EdgeInsets.only(top: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            AnimatedDefaultTextStyle(
+              style: notifier.textStyle,
+              curve: Curves.linear,
+              duration: const Duration(milliseconds: 250),
+              child: const Text("AnimatedDefaultTextStyle"),
+            ),
+            const Padding(padding: EdgeInsets.only(top: 12)),
+            MaterialButton(
+              onPressed: () => notifier.playAnimatedDefaultTextStyle(),
+              color: Colors.blue,
+              child: const Text("start play"),
+            )
+          ],
+        ),
+      );
+    });
+  }
 }
 
 class _MyValuesNotifier extends ChangeNotifier {
   Alignment alignment = Alignment.topLeft;
   double positionLeft = 10;
+  double positionStart = 10;
+  double opacity = 1;
+  static TextStyle defTextStyle = const TextStyle(color: Colors.white, fontSize: 12);
+  static TextStyle defTextStyle2 = const TextStyle(color: Colors.blue, fontSize: 13);
+  TextStyle textStyle = defTextStyle;
 
-  void playAlignAim() {
+  void playAnimatedAlign() {
     alignment = alignment == Alignment.topLeft ? Alignment.topRight : Alignment.topLeft;
     notifyListeners();
   }
 
-  void playAnimatedPositionedAnim() {
-    positionLeft = positionLeft == 10 ? 220 : 10;
+  void playAnimatedPositioned() {
+    positionLeft = positionLeft == 10 ? 260 : 10;
+    notifyListeners();
+  }
+
+  void playAnimatedPositionedStart() {
+    positionStart = positionStart == 10 ? 260 : 10;
+    notifyListeners();
+  }
+
+  void playAnimatedOpacity() {
+    opacity = opacity == 1 ? 0.1 : 1;
+    notifyListeners();
+  }
+
+  void playAnimatedDefaultTextStyle() {
+    textStyle = textStyle == defTextStyle ? defTextStyle2 : defTextStyle;
     notifyListeners();
   }
 }

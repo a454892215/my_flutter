e17837eec49a943b84483a2ffeb79c32da748691
Your Name <1573802540@qq.com>
2022-10-28 10:12:53 +0800

    语言国际化配置

--- lib/flutter_learn/other/datetime_range_sample.dart
+++ lib/flutter_learn/other/datetime_range_sample.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:my_flutter_lib_3/util/toast_util.dart';
+
+import '../../util/Log.dart';
 
 String summary = '''
 ''';
 
 void main() {
   runApp(const MaterialApp(
+    /// 国际化配置=== start=====
+    locale: Locale("zh", "CH"),
+    supportedLocales: [Locale("zh", "CH")],
+    localizationsDelegates: [
+      GlobalMaterialLocalizations.delegate,
+      GlobalWidgetsLocalizations.delegate,
+    ],
+
+    /// 国际化配置=== end=====
     title: "MaterialApp",
     home: _Page(),
   ));
@@ -30,8 +42,8 @@ class _State extends State {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Toast.show("FloatingActionButton");
+          showDateTimeSelect();
         },
-
         child: const Text("按钮"),
       ),
       body: Container(
@@ -46,5 +58,17 @@ class _State extends State {
         ),
       ),
     );
+  }
+
+  /// 异步函数，不确定什么时候返回，而且不能阻止线程...
+  void showDateTimeSelect() async {
+    var ret = await showDateRangePicker(
+      locale: const Locale("zh", "CH"),
+      context: context,
+      firstDate: DateTime(2020, 1),
+      lastDate: DateTime(2022, 1),
+    );
+    Toast.show("$ret");
+    Log.d("ret:$ret");
   }
 }

743aeeef4d099029c47095c72860eeda2a7f0693
Your Name <1573802540@qq.com>
2022-10-28 09:43:40 +0800

    page template code

---
+++ lib/flutter_learn/other/datetime_range_sample.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:my_flutter_lib_3/util/toast_util.dart';
+
+String summary = '''
+''';
+
+void main() {
+  runApp(const MaterialApp(
+    title: "MaterialApp",
+    home: _Page(),
+  ));
+}
+
+class _Page extends StatefulWidget {
+  const _Page();
+
+  @override
+  State<StatefulWidget> createState() {
+    return _State();
+  }
+}
+
+class _State extends State {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Sample"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Toast.show("FloatingActionButton");
+        },
+
+        child: const Text("按钮"),
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.grey,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Container(width: 120, height: 120, color: Colors.blue),
+          ],
+        ),
+      ),
+    );
+  }
+}

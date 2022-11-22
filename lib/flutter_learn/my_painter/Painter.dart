ed2cc12fe140db09c0747c3573809c169eda9ade
Your Name <1573802540@qq.com>
2022-11-22 20:56:52 +0800

    canvas

--- lib/flutter_learn/my_painter/Painter.dart
+++ lib/flutter_learn/my_painter/Painter.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class MyPainter1 extends CustomPainter {
@@ -9,6 +11,16 @@ class MyPainter1 extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     canvas.drawRect(_rect, _myPaint);
+    // 绘制文本
+    drawTextSample(size, canvas);
+  }
+
+  void drawTextSample(Size size, Canvas canvas) {
+    // 绘制文本
+    ParagraphBuilder paragraphBuilder = ParagraphBuilder(ParagraphStyle())..addText('Hello World');
+    ParagraphConstraints paragraphConstraints = ParagraphConstraints(width: size.width);
+    Paragraph paragraph = paragraphBuilder.build() ..layout(paragraphConstraints);
+    canvas.drawParagraph(paragraph, const Offset(10, 10));
   }
 
   @override

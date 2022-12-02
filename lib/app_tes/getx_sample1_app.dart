e912f0d11069570128e8a8e34e6071c5154d55df
Sam <unkown@gmail.com>
2022-12-02 11:14:53 +0800

    示例优化

--- lib/app_tes/getx_sample1_app.dart
+++ lib/app_tes/getx_sample1_app.dart
@@ -1,90 +1,62 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 
+import '../util/Log.dart';
+
 String summary = '''
 Getx 的 GetPage， Bindings， GetxController使用示例：               
   1. 有名路由注册Bindings子类对象,  Bindings 子类注册多个 GetxController 示例
   2. 无名路由绑定 Bindings 跳转页面 示例  
 ''';
 
-
 void main() {
   runApp(GetMaterialApp(
     initialRoute: '/home',
     getPages: [
-      GetPage(name: '/home', page: () => const _First(), binding: SampleBind()),
+      GetPage(name: '/home', page: () => _Page(), binding: SampleBind()),
     ],
   ));
 }
 
-class _First extends StatelessWidget {
-  const _First();
+class _Page extends StatelessWidget {
+  /// 1. 已经在路由中绑定类Controller3，所以直接通过Controller3获取该对象即可
+  final controller = Get.find<Controller3>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Getx 的 Bindings 使用示例"),
-      ),
-      body: ListView(children: [
-        MaterialButton(onPressed: () {
-          /// 2. 无名路由绑定 Bindings 跳转页面
-          Get.to(const _Second(), binding: SampleBind());
-        }, child: const Text("to Second Page")),
-      ],)
-    );
+        appBar: AppBar(
+          title: const Text("Second Page"),
+        ),
+        body: ListView(
+          children: [
+            GetBuilder<Controller3>(builder: (controller) {
+              Log.d("========GetBuilder========");
+              return MaterialButton(
+                onPressed: () {
+                  controller.increment();
+                },
+                child: Text("按钮---${controller.count1}"),
+              );
+            })
+          ],
+        ));
   }
 }
 
-class _Second extends StatelessWidget {
-  const _Second();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Second Page"),
-      ),
-      body: ListView(children: const [
-
-      ],)
-    );
-  }
-}
-
-/// 1. Bindings 注册多个 GetxController 用例
+/// 1. Bindings 注册 GetxController 用例
 class SampleBind extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<Controller>(() => Controller());
-    Get.lazyPut<Controller2>(() => Controller2());
     Get.lazyPut<Controller3>(() => Controller3());
   }
 }
 
-class Controller extends GetxController {
-  var count1 = 0.obs;
+class Controller3 extends GetxController {
+  var count1 = 3;
 
   void increment() {
     count1++;
-    update();
-  }
-}
-
-class Controller2 extends GetxController {
-  var count2 = 0.obs;
-
-  void increment() {
-    count2++;
-    update();
-  }
-}
-
-class Controller3 extends GetxController {
-  var count3 = 0.obs;
-
-  void increment() {
-    count3++;
     update();
   }
 }

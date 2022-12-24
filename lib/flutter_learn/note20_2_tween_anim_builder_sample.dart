2dd17593aac4c2707abaa4d2fe43502d8f3b82eb
Sam <unkown@gmail.com>
2022-12-24 11:44:20 +0800

    anim

--- lib/flutter_learn/note20_2_tween_anim_builder_sample.dart
+++ lib/flutter_learn/note20_2_tween_anim_builder_sample.dart
@@ -12,20 +12,67 @@ class TweenAnimationBuilderTestPage extends StatefulWidget {
   }
 }
 
-class _State extends State {
+class _State extends State with TickerProviderStateMixin {
   ValueNotifier<int> notifier = ValueNotifier<int>(0);
+  late final AnimationController controller1 = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 1000),
+    reverseDuration: const Duration(milliseconds: 1000),
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text("TweenAnimationBuilder-示例"),
+          title: const Text("TweenAnimationBuilder等-示例"),
         ),
         body: ListView(
           children: [
             buildAnim1(),
+            _buildPositionedTransition(),
           ],
         ));
+  }
+
+  Widget _buildPositionedTransition() {
+    return GestureDetector(
+      onTap: () {
+        if(controller1.isCompleted){
+          Log.d("======isCompleted=======");
+          controller1.reverse();
+        }else if(controller1.isDismissed){
+          Log.d("======isDismissed=======");
+          controller1.forward();
+        }
+
+      },
+      child: Container(
+        height: 150,
+        color: Colors.grey,
+        child: Stack(
+          children: [
+            PositionedTransition(
+                rect: RelativeRectTween(
+                  begin: const RelativeRect.fromLTRB(0, 0, 0, 0),
+                  end: const RelativeRect.fromLTRB(70, 0, 0, 0),
+                ).animate(CurvedAnimation(
+                  parent: controller1,
+                  curve: Curves.linear,
+                )),
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Container(
+                    width: 160,
+                    height: 60,
+                    color: Colors.purple,
+                    alignment: Alignment.center,
+                    child: const Text("PositionedTransition示例"),
+                  ),
+                ))
+          ],
+        ),
+      ),
+    );
   }
 
   ColorTween colorTween = ColorTween(begin: const Color(0xff2865d7), end: const Color(0xfffca2ed));
@@ -41,6 +88,7 @@ class _State extends State {
         builder: (context, value, child) {
           return TweenAnimationBuilder(
             key: GlobalKey(),
+
             /// 需要每次不同key 刷新动画才能生效
             builder: (BuildContext context, double value, Widget? child) {
               return buildAnimChild(value);

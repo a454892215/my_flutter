102e512a92d57624b88d7a5703d7b4ca2606c86c
henry <henry@gmail.com>
2023-01-04 16:31:13 +0800

    -

--- lib/flutter_learn/note35_exception.dart
+++ lib/flutter_learn/note35_exception.dart
@@ -33,12 +33,6 @@ class _State extends State<ExceptionTestPage> {
             child: const Text("异常处理测试"),
           ),
           CupertinoButton(
-              child: const Text("flutter 计算异常"),
-              onPressed: () {
-                double a = 10 / 0; // flutter 中除以0 不会报异常
-                toast("a:$a");
-              }),
-          CupertinoButton(
               child: const Text("flutter 空指针异常"),
               onPressed: () {
                 List? ls;
@@ -50,15 +44,13 @@ class _State extends State<ExceptionTestPage> {
                 showDialog(
                     context: context,
                     builder: (BuildContext context) {
-                      return Container(
-                        child: Align(
-                          alignment: Alignment.bottomCenter,
-                          child: Container(
-                            width: 200,
-                            height: 200,
-                            color: Colors.green,
-                            decoration: const BoxDecoration(color: Colors.red),
-                          ),
+                      return Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Container(
+                          width: 200,
+                          height: 200,
+                          color: Colors.green,
+                          decoration: const BoxDecoration(color: Colors.red),
                         ),
                       );
                     });

1d08f1fc987c158c5005510518a06ba21cdad30a
henry <henry@gmail.com>
2023-01-04 19:21:27 +0800

    异常信息格式优化

--- lib/globe_exception_catch.dart
+++ lib/globe_exception_catch.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:my_flutter_lib_3/util/Log.dart';
+import 'package:path_provider/path_provider.dart';
 
 typedef VoidCallback = void Function();
 
@@ -21,9 +23,23 @@ class GlobeExceptionHandler {
   }
 
   void reportException(FlutterErrorDetails details, int type) {
-    final errorMsg = {"exception": details.exceptionAsString(), "stackTrace": details.stack.toString()};
-
-    /// TODO 上报错误
+    final errorMsg = 'exception: ${details.exceptionAsString()} \r\n${details.stack.toString()}';
+    saveLogToLocal(errorMsg);
     Log.e("Not catch Exception type: $type: $errorMsg");
+  }
+
+  Future<void> saveLogToLocal(String log) async {
+    Directory tempDir = await getTemporaryDirectory();
+    String filePath = tempDir.path;
+    File file = File('$filePath/ExceptionLog.txt');
+    bool isExist = await file.exists();
+    if (!isExist) {
+      file = await file.create();
+      isExist = await file.exists();
+      Log.d('filePath: exception log file is not exist and has created finished：$isExist');
+    }
+    log = '\r\n\r\n${DateTime.now()}\r\n$log';
+    file.writeAsString(log, mode: FileMode.append);
+    Log.d("exception log has saved to local，path: ${file.path}");
   }
 }

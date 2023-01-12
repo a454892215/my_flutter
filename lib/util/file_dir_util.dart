ac3b395f0b900baa46ee693bba521225a027df72
henry <henry@gmail.com>
2023-01-12 18:35:48 +0800

    -

--- lib/util/file_dir_util.dart
+++ lib/util/file_dir_util.dart
@@ -1,33 +1,39 @@
-import 'dart:io';
-
-import 'package:path_provider/path_provider.dart';
+import 'package:flutter/foundation.dart';
 import 'package:path/path.dart' as my_path;
+import 'package:path_provider/path_provider.dart';
+main() {}
 
-main() {
-
+bool isWeb() {
+  return kIsWeb == true;
 }
 
 class FileU {
-  static String defDir = "";
 
-  static Future<Directory> getApkSaveDirPath() async {
+  static Future<dynamic> getApkSaveDirPath() async {
     return getTemporaryDirectoryPath();
   }
 
   /// 1. 获取临时目录
-  static Future<Directory> getTemporaryDirectoryPath() async {
+  static Future<dynamic> getTemporaryDirectoryPath() async {
+    if (isWeb()) {
+      return getWebLocalPath();
+    } else {
       return getTemporaryDirectory();
+    }
   }
 
   /// 2. 获取app doc目录
-  static Future<String> getApplicationDocumentsDirectoryPath() async {
-    // Directory directory = await getApplicationDocumentsDirectory();
-    // return directory.path;
-    return defDir;
+  static Future<dynamic> getApplicationDocumentsDirectoryPath() async {
+    if (isWeb()) {
+      return getWebLocalPath();
+    } else {
+      return getApplicationDocumentsDirectory();
+    }
   }
 
   /// 2. 获取web 本地下载目录
   static Future<String> getWebLocalPath() async {
     return my_path.absolute("build${my_path.separator}");
   }
+
 }

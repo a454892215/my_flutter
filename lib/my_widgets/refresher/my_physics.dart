9f89f5b9ee15d426554dbdaa00fdb0efe528aa66
Your Name <1573802540@qq.com>
2023-01-21 08:44:01 +0800

    优化

--- lib/my_widgets/refresher/my_physics.dart
+++ lib/my_widgets/refresher/my_physics.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'dart:math' as math;
 
 import '../../util/Log.dart';
@@ -112,7 +111,7 @@ class MyBouncingScrollPhysics extends ScrollPhysics {
 }
 
 //ignore: must_be_immutable
-class RefresherClampingScrollPhysics extends ScrollPhysics {
+class RefresherClampingScrollPhysics extends ClampingScrollPhysics {
   RefresherClampingScrollPhysics({ScrollPhysics? parent}) : super(parent: parent);
   bool _scrollEnable = true;
 
@@ -136,74 +135,5 @@ class RefresherClampingScrollPhysics extends ScrollPhysics {
       return offset;
     }
     return 0;
-  }
-
-  @override
-  double applyBoundaryConditions(ScrollMetrics position, double value) {
-    assert(() {
-      if (value == position.pixels) {
-        throw FlutterError.fromParts(<DiagnosticsNode>[
-          ErrorSummary('$runtimeType.applyBoundaryConditions() was called redundantly.'),
-          ErrorDescription(
-            'The proposed new position, $value, is exactly equal to the current position of the '
-            'given ${position.runtimeType}, ${position.pixels}.\n'
-            'The applyBoundaryConditions method should only be called when the value is '
-            'going to actually change the pixels, otherwise it is redundant.',
-          ),
-          DiagnosticsProperty<ScrollPhysics>('The physics object in question was', this, style: DiagnosticsTreeStyle.errorProperty),
-          DiagnosticsProperty<ScrollMetrics>('The position object in question was', position, style: DiagnosticsTreeStyle.errorProperty),
-        ]);
-      }
-      return true;
-    }());
-    if (value < position.pixels && position.pixels <= position.minScrollExtent) {
-      return value - position.pixels;
-    }
-    if (position.maxScrollExtent <= position.pixels && position.pixels < value) {
-      return value - position.pixels;
-    }
-    if (value < position.minScrollExtent && position.minScrollExtent < position.pixels) {
-      return value - position.minScrollExtent;
-    }
-    if (position.pixels < position.maxScrollExtent && position.maxScrollExtent < value) {
-      return value - position.maxScrollExtent;
-    }
-    return 0.0;
-  }
-
-  @override
-  Simulation? createBallisticSimulation(ScrollMetrics position, double velocity) {
-    final Tolerance tolerance = this.tolerance;
-    if (position.outOfRange) {
-      double? end;
-      if (position.pixels > position.maxScrollExtent) {
-        end = position.maxScrollExtent;
-      }
-      if (position.pixels < position.minScrollExtent) {
-        end = position.minScrollExtent;
-      }
-      assert(end != null);
-      return ScrollSpringSimulation(
-        spring,
-        position.pixels,
-        end!,
-        math.min(0.0, velocity),
-        tolerance: tolerance,
-      );
-    }
-    if (velocity.abs() < tolerance.velocity) {
-      return null;
-    }
-    if (velocity > 0.0 && position.pixels >= position.maxScrollExtent) {
-      return null;
-    }
-    if (velocity < 0.0 && position.pixels <= position.minScrollExtent) {
-      return null;
-    }
-    return ClampingScrollSimulation(
-      position: position.pixels,
-      velocity: velocity,
-      tolerance: tolerance,
-    );
   }
 }

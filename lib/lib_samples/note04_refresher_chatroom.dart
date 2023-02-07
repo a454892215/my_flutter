f837b1b756b7d800365b00350a554070f20b7880
henry <henry@gmail.com>
2023-02-07 20:16:59 +0800

    refresh 示例

---
+++ lib/lib_samples/note04_refresher_chatroom.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:my_flutter_lib_3/my_widgets/refresher/refresher.dart';
+import '../my_widgets/refresher/my_physics.dart';
+import '../my_widgets/refresher/refresh_state.dart';
+
+class RefresherCharRoomPage extends StatefulWidget {
+  const RefresherCharRoomPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _SamplePageState();
+  }
+}
+
+class _SamplePageState extends State with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    super.initState();
+    refreshDataForList(8);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return buildScaffold();
+  }
+
+  ScrollController sc = ScrollController();
+
+  Scaffold buildScaffold() {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("SmartRefresher-聊天室模式"),
+      ),
+      body: Container(
+        color: Colors.grey[200],
+        child: Column(
+          children: [
+            Expanded(child: LayoutBuilder(
+              builder: (context, constraint) {
+                double refresherContentHeight = constraint.maxHeight;
+                double refresherContentWidth = constraint.maxWidth;
+                return Refresher(
+                    sc: sc,
+                    height: refresherContentHeight,
+                    width: refresherContentWidth,
+                    isReverseScroll: true,
+                    controller: RefresherController(),
+                    headerFnc: RefresherFunc.load_more,
+                    footerFnc: RefresherFunc.refresh,
+                    onHeaderLoad: (state) async {
+                      await Future.delayed(const Duration(milliseconds: 1000));
+                      loadMoreDataForList(10);
+                      listView2Notifier.value++;
+                      state.notifyHeaderLoadFinish();
+                    },
+                    onFooterLoad: (state) async {
+                      await Future.delayed(const Duration(milliseconds: 1000));
+                      refreshDataForList(10);
+                      listView2Notifier.value++;
+                      state.notifyFooterLoadFinish();
+                    },
+                    child: Container(
+                      height: refresherContentHeight,
+                      width: refresherContentWidth,
+                      color: const Color(0xffaeeeae),
+                      //   padding: const EdgeInsets.all(10),
+                      child: buildListView2(sc),
+                    ));
+              },
+            )),
+          ],
+        ),
+      ),
+    );
+  }
+
+  var list2 = [];
+  late final ValueNotifier<int> listView2Notifier = ValueNotifier<int>(0);
+
+  /// 3. 带有分割线的 按需加载： ListView.separated
+  Widget buildListView2(ScrollController sc) {
+    return ValueListenableBuilder(
+        valueListenable: listView2Notifier,
+        builder: (a, b, c) {
+          return ListView.separated(
+              itemCount: list2.length,
+              physics: RefresherClampingScrollPhysics(),
+              shrinkWrap: false,
+              reverse: true,
+              controller: sc,
+              separatorBuilder: (BuildContext context, int index) {
+                return Container(
+                  color: Colors.red,
+                  height: 5,
+                );
+              },
+              itemBuilder: (BuildContext context, int index) {
+                // Log.d("itemBuilder index: $index");
+                String text = "文本$index";
+                return Container(
+                  height: 60,
+                  color: const Color(0xffb9e8b9),
+                  alignment: Alignment.center,
+                  child: Text(text),
+                );
+              });
+        });
+  }
+
+  void loadMoreDataForList(int size) {
+    for (int i = 0; i < size; i++) {
+      list2.add("data");
+    }
+  }
+
+  void refreshDataForList(int size) {
+    list2.clear();
+    for (int i = 0; i < size; i++) {
+      list2.add("data");
+    }
+  }
+}
--- lib/pages/flutter_lib_api_samples.dart
+++ lib/pages/flutter_lib_api_samples.dart
@@ -5,6 +5,8 @@ import '../lib_samples/note01_picker.dart';
 import '../lib_samples/note02_screenshot_event.dart';
 import '../lib_samples/note03_web_parser.dart';
 
+import '../lib_samples/note04_refresher_chatroom.dart';
+import '../lib_samples/note04_refresher_normal.dart';
 import '../util/Log.dart';
 
 ///Navigator.of(context).pushNamed("/page2"); 这种方式跳转页面是整个页面的跳转
@@ -41,6 +43,8 @@ class Page2State extends State {
                   MaterialButton(child: const Text("picker库用例"), onPressed: () => Get.to(() => const PickerSamplePage())),
                   MaterialButton(child: const Text("ScreenShotEvent用例"), onPressed: () => Get.to(() => const ScreenShotEventSamplePage())),
                   MaterialButton(child: const Text("WebParserPage"), onPressed: () => Get.to(() => WebParserPage())),
+                  MaterialButton(child: const Text("refresher-正常模式"), onPressed: () => Get.to(() => const RefresherNormalSamplePage())),
+                  MaterialButton(child: const Text("refresher-聊天室模式"), onPressed: () => Get.to(() => const RefresherCharRoomPage())),
                 ],
               )),
         ));

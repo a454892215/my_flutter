060bc5d249b5c625ab57cd4ce23146674c923bbe
henry <henry@gmail.com>
2023-02-17 12:45:31 +0800

    RouterAware-生命周期函数标注

--- lib/flutter_learn/note34_route_aware.dart
+++ lib/flutter_learn/note34_route_aware.dart
@@ -1,9 +1,11 @@
-import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
+import '../lib_samples/note08_reskin.dart';
 import '../material_apps.dart';
 import '../util/Log.dart';
 
@@ -29,36 +31,38 @@ class _State extends State<RouteAwareTestPage> with RouteAware {
     super.initState();
   }
 
+  /// 第一次进入执行
   @override
   void didChangeDependencies() {
-    Log.d("=====didChangeDependencies====== $widget");
+    Log.d("===第一次进入执行==didChangeDependencies====== $widget");
     routeObserver.subscribe(this, ModalRoute.of(context) as PageRoute); //Subscribe it here
     super.didChangeDependencies();
   }
 
   @override
   void didPopNext() {
-    Log.d('=========didPopNext============');
+    Log.d('=========didPopNext====回到此页面执行===== currentRoute:${Get.currentRoute} ===');
   }
 
+  /// 第一次进入执行
   @override
   void didPush() {
-    Log.d('=========didPush============');
+    Log.d('=========didPush===第一次进入执行==== currentRoute:${Get.currentRoute} =======');
   }
 
   @override
   void didPop() {
-    Log.d('=========didPop============');
+    Log.d('=========didPop====退出此页面执行========');
   }
 
   @override
   void didPushNext() {
-    Log.d('=========didPushNext============');
+    Log.d('=========didPushNext=====离开此页面执行=== currentRoute:${Get.currentRoute} ====');
   }
 
   @override
   void dispose() {
-    Log.d('=========dispose============');
+    Log.d('=========dispose=======销毁此页面执行=====');
     super.dispose();
     routeObserver.unsubscribe(this);
   }
@@ -72,16 +76,21 @@ class _State extends State<RouteAwareTestPage> with RouteAware {
     ));
     return Scaffold(
         body: SafeArea(
-          child: ListView(
-            children: [
-              Container(
-                color: Colors.green,
-                height: 150.w,
-                alignment: Alignment.center,
-                child: const Text("RouteAwareTestPage"),
-              )
-            ],
-          ),
-        ));
+      child: ListView(
+        children: [
+          Container(
+            color: Colors.green,
+            height: 150.w,
+            alignment: Alignment.center,
+            child: CupertinoButton(
+              onPressed: () {
+                Get.to(() => const ChangeSkinPage());
+              },
+              child: const Text("去换肤页面"),
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }

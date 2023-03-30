fba71ba332b8ed92e6549e562434212613f472c0
henry <henry@gmail.com>
2023-03-30 12:29:02 +0800

    -

--- lib/util/Log.dart
+++ lib/util/Log.dart
@@ -1,15 +1,14 @@
-// import 'package:flutter/material.dart';
 
 import 'package:logger/logger.dart';
 
-import '../env.dart';
 
 class Log {
   static const String tag = "LLpp:";
   static bool isDebug = false;
-  static bool debugEnable = EnvironmentConfig.isDebug();
+  static bool debugEnable = isDebugMode();
   static var logger = Logger(
-    printer: MyLogPrinter(),
+    // printer: MyLogPrinter(),
+    printer: SimplePrinter(),
   );
 
   static bool isDebugMode() {
@@ -17,12 +16,13 @@ class Log {
       isDebug = true;
       return true;
     }());
+    print("LLpp 是否debug 环境：$isDebug");
     return isDebug;
   }
 
-  static void d(dynamic msg) {
+  static void d(dynamic msg, {int traceDepth = 1}) {
     if (debugEnable) {
-      _print(Level.debug, msg);
+      _print(Level.debug, msg, traceDepth: traceDepth);
     }
   }
 
@@ -31,34 +31,55 @@ class Log {
   }
 
   static void w(dynamic msg) {
-    _print(Level.error, msg);
+    _print(Level.warning, msg);
   }
 
   static void e(dynamic msg) {
     _print(Level.error, msg);
   }
 
-  static void _print(Level level, dynamic msg) {
-    String traceInfo = getTraceInfo();
+  static void _print(Level level, dynamic msg, {int traceDepth = 1}) {
+    String traceInfo = getTraceInfo(level, traceDepth: traceDepth);
     logger.log(level, "${DateTime.now()} $traceInfo $tag$msg");
   }
 
-  static String getTraceInfo() {
-    var traceList = StackTrace.current.toString().replaceAll(RegExp(r"(\s\s\s\s)+"), "    ").split("\n");
-    String pre = traceList[0];
-    String traceInfo = '未定位到调用位置';
-    int end = traceList.length > 6 ? 6 : traceList.length;
-    for (int i = 1; i < end; i++) {
-      var cur = traceList[i];
-      if (pre.contains("Log.") && !cur.contains("Log.")) {
-        traceInfo = cur;
-        break;
+  static String getTraceInfo(Level level, {int traceDepth = 1}) {
+    try {
+      var traceList = StackTrace.current.toString().replaceAll(RegExp(r"(\s\s\s\s)+"), "    ").split("\n");
+      String pre = traceList[0];
+      String traceInfo = '未定位到调用位置';
+      int end = traceList.length > 6 ? 6 : traceList.length;
+      List<String> tarTraceList = [];
+      for (int i = 1; i < end; i++) {
+        var cur = traceList[i];
+        if (pre.contains("Log.") && !cur.contains("Log.")) {
+          traceInfo = cur;
+          int end2 = i + traceDepth;
+          end2 = end2 > traceList.length ? traceList.length : end2;
+          for (int j = i; j < end2; j++) {
+            var cur = traceList[j];
+            tarTraceList.add(cur);
+          }
+          break;
+        }
+        pre = traceList[i];
       }
-      pre = traceList[i];
+      String ret = "";
+      var tarSize = tarTraceList.length;
+      for (int i = 0; i < tarSize; i++) {
+        String item = tarTraceList[i];
+        int start = item.indexOf('(package:');
+        if(start > -1){
+          item = item.substring(start);
+        }
+        String delimiter = i == tarSize - 1 ? "" : '\r\n';
+        ret += '$item$delimiter';
+      }
+      return ret.isEmpty ? traceInfo : ret;
+    } catch (e, trace) {
+      logger.log(level, "${DateTime.now()} $e $tag $trace");
     }
-    var start = traceInfo.indexOf('(package:');
-    traceInfo = traceInfo.substring(start);
-    return traceInfo;
+    return "定位调用栈发生异常";
   }
 }
 

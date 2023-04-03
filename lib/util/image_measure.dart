7404400e1d21bd0e979b1e1993e286500ad361c4
henry <henry@gmail.com>
2023-04-03 15:14:06 +0800

    图片大小测量工具类

--- lib/util/image_measure.dart
+++ lib/util/image_measure.dart
@@ -2,24 +2,35 @@ import 'package:flutter/cupertino.dart';
 
 typedef SizeCallback = void Function(Size size);
 
-void getNetworkImageSize(String imageURL, SizeCallback callback) {
-  Image image = Image.network(imageURL);
-  listenerSize(image, callback);
-}
+class ImageMeasureHelper {
+  ImageStream? stream;
+  final SizeCallback callback;
+  late ImageStreamListener listener = ImageStreamListener(onListener);
 
-void getAssertImageSize(String assertPath, SizeCallback callback) {
-  Image image = Image.asset(assertPath);
-  listenerSize(image, callback);
-}
+  ImageMeasureHelper(this.callback);
 
-void listenerSize(Image image, SizeCallback callback) {
-  image.image.resolve(const ImageConfiguration()).addListener(
-    ImageStreamListener(
-      (ImageInfo image, bool synchronousCall) {
-        var myImage = image.image;
-        Size imageSize = Size(myImage.width.toDouble(), myImage.height.toDouble());
-        callback(imageSize);
-      },
-    ),
-  );
+  void getNetworkImageSize(String imageURL) {
+    Image image = Image.network(imageURL);
+    listenerSize(image);
+  }
+
+  void getAssertImageSize(String assertPath) {
+    Image image = Image.asset(assertPath);
+    listenerSize(image);
+  }
+
+  void listenerSize(Image image) {
+    stream = image.image.resolve(const ImageConfiguration());
+    stream?.addListener(listener);
+  }
+
+  void onListener(ImageInfo image, bool synchronousCall) {
+    var myImage = image.image;
+    Size imageSize = Size(myImage.width.toDouble(), myImage.height.toDouble());
+    callback(imageSize);
+  }
+
+  void close() {
+    stream?.removeListener(listener);
+  }
 }
